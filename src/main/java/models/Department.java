0b536a3e314f5eadf5af6c5e3b6c76fea975b787
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-21 11:04:42 +0300

    add department class

---
+++ src/main/java/models/Department.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.util.Objects;
+
+public class Department {
+    private String name;
+    private String description;
+    private int totalEmployees;
+    private int id;
+}
