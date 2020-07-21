ca2ea73c57511f631b502d20b3ae822381c1bc24
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-21 11:11:41 +0300

    add constructor, getters and setters

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -3,4 +3,11 @@ package models;
 import java.util.Objects;
 
 public class News {
+    private String title;
+    private String description;
+    private String type;
+    private String author;
+    private int id;
+
+    
 }
