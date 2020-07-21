f260b4565050a7f0a29ea5ac443060d3618e99a2
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-21 14:00:53 +0300

    add sql2oDeptDao test

--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -16,4 +16,29 @@ public class NewsTest {
     private News altNews(){
         return new News("New Album","burna boy releases new album");
     }
+
+    @Test
+    public void initializeCorrectly(){
+        News news = altNews();
+        assertTrue(news instanceof News);
+    }
+
+    @Test
+    public void getFunctionsWorkCorrectly(){
+        News news = simpleNews();
+        assertEquals(news.getTitle(),simpleNews().getTitle());
+        assertEquals(news.getDescription(),simpleNews().getDescription());
+        assertEquals(news.getType(),simpleNews().getType());
+    }
+
+    @Test
+    public void getDifferentNewsType(){
+        News news = simpleNews();
+        News news2 = altNews();
+        news2.setType("Entertainment");
+        news2.setAuthor("burna");
+        assertEquals(news.getType(),"General");
+        assertEquals(news2.getType(),"Entertainment");
+        assertEquals(news2.getAuthor(),"burna");
+    }
 }
