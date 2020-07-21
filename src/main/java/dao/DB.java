e9b71b490f81aaf1c8dadd845f2c438f988bdd23
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-21 11:50:11 +0300

    add sql2o link to database

--- src/main/java/dao/DB.java
+++ src/main/java/dao/DB.java
@@ -7,4 +7,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DB {
+    private static URI dbUri;
+    public static Sql2o sql2o;
+    static Logger logger = LoggerFactory.getLogger(DB.class);
+    static {
+
+        try {
+            if (System.getenv("DATABASE_URL") == null) {
+                dbUri = new URI("postgres://localhost:5432/news_portal");
+            } else {
+                dbUri = new URI(System.getenv("DATABASE_URL"));
+            }
+            int port = dbUri.getPort();
+            String host = dbUri.getHost();
+            String path = dbUri.getPath();
+            String username = (dbUri.getUserInfo() == null) ? "ramzan" : dbUri.getUserInfo().split(":")[0];
+            String password = (dbUri.getUserInfo() == null) ? "1234" : dbUri.getUserInfo().split(":")[1];
+            sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
+        } catch (URISyntaxException e ) {
+            logger.error("Unable to connect to database.");
+        }
+    }
 }
