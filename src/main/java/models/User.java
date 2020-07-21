a131f4b78d1bb42d94f8967e260c6d7e0914b7bf
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-21 11:43:16 +0300

    add user class

---
+++ src/main/java/models/User.java
@@ -0,0 +1,6 @@
+package models;
+
+import java.util.Objects;
+
+public class User {
+}
--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -1,7 +1,31 @@
 package models;
 
+import dao.DatabaseRule;
+import org.junit.Rule;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class DepartmentTest {
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
+
+    private Department newDept(){
+        return new Department("Producers","We the best");
+    }
+
+    @Test
+    public void initializeCorrectly(){
+        Department department = newDept();
+        assertTrue(department instanceof Department);
+    }
+
+    @Test
+    public void getMethodsWorkCorrectly(){
+        Department department = newDept();
+        assertEquals("Producers",department.getName());
+        assertEquals("We the best",department.getDescription());
+    }
+
 
 }
