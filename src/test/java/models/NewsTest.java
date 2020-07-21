c4af96af7e488bb22c175d6907f681e4795c56f2
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-21 13:57:12 +0300

    add test

--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -1,7 +1,19 @@
 package models;
 
+import dao.DatabaseRule;
+import org.junit.Rule;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class NewsTest {
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
 
+    private News simpleNews(){
+        return new News("Simple News","today is sunny");
+    }
+    private News altNews(){
+        return new News("New Album","burna boy releases new album");
+    }
 }
