f55e70aef7360f024f73702777e8eed0a3d8023b
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-21 15:53:40 +0300

    add newsDao test

--- src/test/java/dao/Sql2oNewsDaoTest.java
+++ src/test/java/dao/Sql2oNewsDaoTest.java
@@ -1,7 +1,113 @@
 package dao;
 
+import models.Department;
+import models.News;
+import models.User;
+import org.junit.Rule;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class Sql2oNewsDaoTest {
+    private Sql2oDeptDao deptDao = new Sql2oDeptDao();
+    private static Sql2oNewsDao newsDao = new Sql2oNewsDao();
+    private static Sql2oUserDao userDao = new Sql2oUserDao();
+
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
+
+    private News altNews(){
+        News news = new News("New Album","burna drop new album");
+        news.setAuthor(newUser().getName());
+        newsDao.add(news);
+        return news;
+    }
+    private News altNews2(){
+        News news = new News("New Album Yet Again","burna drop new album again");
+        news.setType("entertainment");
+        news.setAuthor(newUser2().getName());
+        newsDao.add(news);
+        return news;
+    }
+
+    private Department newDept(){
+        Department department = new Department("Entertainment","We the best");
+        deptDao.add(department);
+        return department;
+    }
+
+    private User newUser(){
+        User user = new User("Burna Boy","Manager","Artist","Media");
+        userDao.add(user);
+        return user;
+    }
+
+    private User newUser2(){
+        User user = new User("John Wick","assistant","Actor","Human Resource");
+        userDao.add(user);
+        return user;
+    }
+
+    @Test
+    public void newsGetsSavedToDb(){
+        News news = altNews();
+        assertNotEquals(0,news.getId());
+    }
+
+    @Test
+    public void findNewsById(){
+        News news = altNews();
+        News news2 = altNews2();
+        assertTrue(news.equals(newsDao.findById(news.getId())));
+    }
+
+
+    @Test
+    public void getDepartmentAfterCrosscheck_String(){
+        Department department = newDept();
+        News news2 = altNews2();
+        assertEquals("Entertainment",newsDao.findById(news2.getId()).getType());
+    }
+
+    @Test
+    public void findAllNewsPosts_int(){
+        News news = altNews();
+        News news2 = altNews2();
+        assertEquals(2,newsDao.allNews().size());
+    }
+
+    @Test
+    public void simpleDeleteNewsById_true(){
+        News news = altNews();
+        News news2 = altNews2();
+        newsDao.deleteById(news.getId());
+        assertEquals(1,newsDao.allNews().size());
+    }
+
+    @Test
+    public void deleteAllNewsPosts(){
+        News news = altNews();
+        News news2 = altNews2();
+        newsDao.deleteAll();
+        assertEquals(0,newsDao.allNews().size());
+    }
+
+    @Test
+    public void addNewsToGeneralDepartment(){
+        User user = newUser();
+        News news = altNews();
+        newsDao.addNewsToDepartment(0,news.getId(),user.getId());
+        assertEquals(1,deptDao.allDepartmentNews(0).size());
+        assertEquals("General",deptDao.allDepartmentNews(0).get(0).getType());
+    }
+
+    @Test
+    public void addNewsToSpecificDepartment(){
+        Department department = newDept();
+        User user = newUser();
+        News news = altNews2();
+        newsDao.addNewsToDepartment(department.getId(),news.getId(),user.getId());
+        assertEquals(1,deptDao.allDepartmentNews(department.getId()).size());
+    }
 
 }
--- src/test/java/dao/Sql2oUserDaoTest.java
+++ src/test/java/dao/Sql2oUserDaoTest.java
@@ -1,7 +1,66 @@
 package dao;
 
+import models.User;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.sql2o.Sql2o;
+
 import static org.junit.Assert.*;
 
 public class Sql2oUserDaoTest {
+    private static Sql2oUserDao userDao = new Sql2oUserDao();
+
+    @Rule
+    public DatabaseRule databaseRule = new DatabaseRule();
+
+    private User newUser(){
+        User user = new User("burna Boy","Manager","Artist","Media");
+        userDao.add(user);
+        return user;
+    }
+    private User newUser2(){
+        User user = new User("John Wick","assistant","Actor","Human Resource");
+        userDao.add(user);
+        return user;
+    }
+
+    @Test
+    public void userSavedToDatabase(){
+        User user = newUser();
+        assertNotEquals(0,user.getId());
+    }
+
+    @Test
+    public void findingParticularUser(){
+        User user = newUser();
+        User user2 = newUser2();
+        User foundUser = userDao.findById(user.getId());
+        assertTrue(user.equals(foundUser));
+    }
+
+    @Test
+    public void getAllUsers(){
+        User user = newUser();
+        User user2 = newUser2();
+        assertTrue(userDao.allUsers().contains(user));
+        assertTrue(userDao.allUsers().contains(user2));
+    }
+
+    @Test
+    public void deleteUserFromDatabase_int(){
+        User user = newUser();
+        User user2 = newUser2();
+        userDao.deleteById(user.getId());
+        assertEquals(1, userDao.allUsers().size());
+    }
+
+    @Test
+    public void deleteAllUsers(){
+        User user = newUser();
+        User user2 = newUser2();
+        userDao.deleteAll();
+        assertEquals(0, userDao.allUsers().size());
+    }
 
 }
