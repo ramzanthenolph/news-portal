bbb0e34a9acb2b7a9f07a272f10648d628be72d0
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-21 13:34:07 +0300

    add import models.Department;

---
+++ src/main/java/dao/Sql2oDeptDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import models.Department;
+import models.News;
+import models.User;
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sql2oDeptDao {
+}
