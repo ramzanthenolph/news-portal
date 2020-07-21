02c4aa10bb098daee12ecaf17cd75dcabef9f360
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-21 13:53:20 +0300

    add tests

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 
 public class UserTest {
     @Rule
-    public DatabaRule databaseRule = new DatabaseRule();
+    public DatabaseRule databaseRule = new DatabaseRule();
 
     private User newUser(){
         return new User("Kanye West","Manager","Artist","Media");
