21405669fb9aad049a0271204eff3fc43961a592
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-21 11:39:17 +0300

    add test files

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -9,5 +9,65 @@ public class News {
     private String author;
     private int id;
 
-    
+    public News(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.type = "General";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        News news = (News) o;
+        return id == news.id &&
+                Objects.equals(title, news.title) &&
+                Objects.equals(description, news.description) &&
+                Objects.equals(type, news.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, description, type, id);
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
