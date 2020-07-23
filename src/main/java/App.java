4613bda1adffbc84288aaf6e0e69ad29e4addfb5
ramzanthenolph <ramzanthenolph@gmail.com>
2020-07-23 15:13:06 +0300

    add heroku configurations

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,29 @@
+import static spark.Spark.*;
+
+import com.google.gson.Gson;
+//import dao.SQL2oSitemapDao;
+import models.*;
+import dao.Sql2oNewsDao;
+import dao.Sql2oDeptDao;
+import dao.Sql2oUserDao;
+
 public class App {
+    public static void main(String[] args){
+        Sql2oDeptDao deptDao = new Sql2oDeptDao();
+        Sql2oUserDao userDao = new Sql2oUserDao();
+        Sql2oNewsDao newsDao = new Sql2oNewsDao();
+        //SQL2oSitemapDao sitemapDao = new SQL2oSitemapDao();
+        Gson gson = new Gson();
+
+        /*heroku config*/
+        ProcessBuilder process = new ProcessBuilder();
+        int port;
+
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+        port(port);
+    }
 }
